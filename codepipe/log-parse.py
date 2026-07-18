80ef6e4cb5b3acebc11448d4d83b9914301bacb4
Akash Kundu <akash.kundu1297@gmail.com>
2026-07-18 14:39:34 +0530

    Create log-parse.py

---
+++ codepipe/log-parse.py
@@ -0,0 +1,18 @@
+endpoint_dict={}
+with open('file.log') as f:
+    for line in f:
+        #print(line)
+        parts=line.strip().split(" ")
+        date=parts[0]
+        timestamp=parts[1]
+        operation=parts[2]
+        endpoint=parts[3]
+        statusCode=parts[4]
+
+        if(endpoint_dict.get(endpoint)):
+            prev=endpoint_dict.get(endpoint)
+            endpoint_dict[endpoint]=1+int(prev)
+        else:
+            endpoint_dict[endpoint]=1
+
+print('Result: ',endpoint_dict)
